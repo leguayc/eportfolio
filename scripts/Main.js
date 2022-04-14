def884f1e3acf4bb485e062acfe52b5a32a6e869
Corentin <corentin.leguay@gmail.com>
2022-04-14 16:49:39 +0200

    Added some text to test and function to compute age

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,5 +1,18 @@
 import { SceneManager } from './SceneManager.js';
 
+// Compute age
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+document.getElementById('age').textContent = getAge('1999/12/17')
+
 /**
  * Base
  */
